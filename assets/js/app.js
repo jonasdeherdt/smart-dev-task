57d28b2b95b0c80548c4b89c85c72b7f33948a2c
Jonas <jonasdeherdt@gmail.com>
2020-06-08 17:52:52 +0200

    enable sass

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,10 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+import '../css/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
+
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
